568b80b73e56f174115fe7e39ef14ae2f0068e63
David Gómez Cano <davidg@ILITIA.COM>
2017-11-03 13:47:01 +0100

    Add project and files.

---
+++ DragAndDropExample/Views/Scenario3Page.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using DragAndDropExample.ViewModels;
+
+using Windows.UI.Xaml.Controls;
+using DragAndDropExample.Models;
+using Windows.ApplicationModel.DataTransfer;
+
+namespace DragAndDropExample.Views
+{
+    public sealed partial class Scenario3Page : Page
+    {
+        public Scenario3ViewModel ViewModel { get; } = new Scenario3ViewModel();
+
+        public Scenario3Page()
+        {
+            InitializeComponent();
+        }
+
+        private void ListView_DragItemsStarting(object sender, DragItemsStartingEventArgs e)
+        {
+            var items = string.Join(",", e.Items.Cast<CustomItem>().Select(i => i.Id));
+            e.Data.SetText(items);
+            e.Data.RequestedOperation = DataPackageOperation.Move;
+        }
+    }
+}
